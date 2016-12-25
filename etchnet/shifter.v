cec7ccd5369d074bd2057ead404b4b0f32b185a4
Mihailo Isakov <mihailo@protonmail.com>
2016-12-24 20:49:51 -0500

    parametrized shifter

---
+++ etchnet/shifter.v
@@ -0,0 +1,29 @@
+module shifter 
+#(
+    parameter shift1 = 8,
+    parameter shift2 = 8
+)
+(
+    input clk,
+    input rst,
+    input data,
+    output out1,
+    output out2
+);
+
+    localparam regsize = (shift1 > shift2) ? shift1 : shift2;
+
+    reg [regsize-1:0] bits;
+
+    always @ (posedge clk) begin
+        if (rst)
+            bits = 0;
+        else begin
+            bits = {bits[regsize-2:0], data};
+        end
+    end
+
+    assign out1 = bits[shift1-1];
+    assign out2 = bits[shift2-1];
+
+endmodule
---
+++ etchnet/t_shifter.v
@@ -0,0 +1,68 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   20:31:27 12/24/2016
+// Design Name:   shifter
+// Module Name:   /home/mihailo/development/projects/etchnet/etchnet/t_shifter.v
+// Project Name:  etchnet
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: shifter
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module t_shifter;
+
+	// Inputs
+	reg clk;
+	reg rst;
+	reg data;
+
+	// Outputs
+	wire out1;
+	wire out2;
+
+	// Instantiate the Unit Under Test (UUT)
+	shifter #(3, 5) uut (
+		.clk(clk), 
+		.rst(rst), 
+		.data(data), 
+		.out1(out1), 
+		.out2(out2)
+	);
+
+    always 
+        #1 clk = ~clk;
+
+    always
+        #2 data = $random%2;
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		rst = 0;
+		data = 0;
+
+        #10 rst = 1;
+        #10 rst = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
